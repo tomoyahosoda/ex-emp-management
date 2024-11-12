9a5779245674dffc342183d328aa55e83e8f7ecf
tomoya.hosoda <tomoya.hosoda@rakus-partners.co.jp>
2024-11-12 13:46:40 +0900

    LoginForm作成

---
+++ src/main/java/com/example/form/LoginForm.java
@@ -0,0 +1,28 @@
+package com.example.form;
+
+/**
+ * ログインフォーム.
+ * @author 細田智也
+ */
+public class LoginForm {
+    /* メールアドレス */
+    private String mailAddress;
+    /* パスワード */
+    private String password;
+    @Override
+    public String toString() {
+        return "LoginForm [mailAddress=" + mailAddress + ", password=" + password + "]";
+    }
+    public String getMailAddress() {
+        return mailAddress;
+    }
+    public void setMailAddress(String mailAddress) {
+        this.mailAddress = mailAddress;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
