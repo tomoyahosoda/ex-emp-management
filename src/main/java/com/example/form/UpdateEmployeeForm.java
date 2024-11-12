59e3061a906eb8792b7448c76e2c6a157b32edfd
tomoya.hosoda <tomoya.hosoda@rakus-partners.co.jp>
2024-11-12 14:53:47 +0900

    Ex04 UpdateEmployeeForm作成

---
+++ src/main/java/com/example/form/UpdateEmployeeForm.java
@@ -0,0 +1,28 @@
+package com.example.form;
+
+/**
+ * 従業員更新フォーム.
+ * @author 細田智也
+ */
+public class UpdateEmployeeForm {
+    /* 従業員ID */
+    private String id;
+    /* 扶養人数 */
+    private String dependentsCount;
+    @Override
+    public String toString() {
+        return "UpdateEmployeeForm [id=" + id + ", dependentsCount=" + dependentsCount + "]";
+    }
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getDependentsCount() {
+        return dependentsCount;
+    }
+    public void setDependentsCount(String dependentsCount) {
+        this.dependentsCount = dependentsCount;
+    }
+}
