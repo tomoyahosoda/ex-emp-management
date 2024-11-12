7330a7724d11d7dffbe3aa3ebeb192d13d3a1a51
tomoya.hosoda <tomoya.hosoda@rakus-partners.co.jp>
2024-11-12 14:51:31 +0900

    Ex04 EmployeeService追加

---
+++ src/main/java/com/example/service/EmployeeService.java
@@ -0,0 +1,29 @@
+package com.example.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.domain.Employee;
+import com.example.repository.EmployeeRepository;
+
+/**
+ * 従業員サービス.
+ * @author 細田智也
+ */
+@Service
+@Transactional
+public class EmployeeService {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    /**
+     * 従業員情報を全件取得.
+     * @return 従業員リスト
+     */
+    public List<Employee> showList() {
+        return employeeRepository.findAll();
+    }
+}
