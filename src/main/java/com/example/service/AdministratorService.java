849b15a8baaeef6bd5632cbcce15f9b2fbc67792
tomoya.hosoda <tomoya.hosoda@rakus-partners.co.jp>
2024-11-12 13:06:48 +0900

    AdmiinstratoServiceにinsertメソッド追加

--- src/main/java/com/example/service/AdministratorService.java
+++ src/main/java/com/example/service/AdministratorService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.example.domain.Administrator;
 import com.example.repository.AdministratorRepository;
 
 /**
@@ -15,4 +16,12 @@ import com.example.repository.AdministratorRepository;
 public class AdministratorService {
     @Autowired
     private AdministratorRepository administratorRepository;
+
+    /**
+     * 管理者情報を挿入.
+     * @param administrator 管理者情報
+     */
+    public void insert(Administrator administrator) {
+        administratorRepository.insert(administrator);
+    }
 }
